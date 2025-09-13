455f95e32024513f49d1bab2f6627df24965e329
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-13 15:06:24 +0100

    add read file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+fn read_file(path: &str) -> Result<String, std::io::Error> {
+    let mut file = std::fs::File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let content = read_file("data/test.txt")?;
+    println!("{}", content);
+    Ok(())
 }
