5f02c062d5ad3cd3e152a9a66f2a4fc7cfb35fd1
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-14 12:39:29 +0100

    add mode enum

--- src/main.rs
+++ src/main.rs
@@ -7,46 +7,25 @@ fn read_file(path: &str) -> Result<String, std::io::Error> {
     Ok(content)
 }
 
-fn print_output(value: usize, file_name: &str) {
-    println!("{} {}", value, file_name)
-}
-
-fn count_bytes(data: &str) -> usize {
-    data.len()
-}
-
-fn count_words(data: &str) -> usize {
-    1
-}
-
-fn count_lines(data: &str) -> usize {
-    1
-}
-
-fn count_chars(data: &str) -> usize {
-    1
-}
-
+enum Mode { All, Bytes, Words, Lines, Chars }
 
 fn main() -> Result<(), std::io::Error> {
   let args: Vec<String> = std::env::args().collect();
 
+  let mut mode = Mode::All;
+  let mut file_name: Option<String> = None;
+
   let mut i = 1;
   while i < args.len() {
     match args[i].as_str() {
-        "-c" => {
-            count_bytes("data/test.txt")
-        },
-        "-w" => {
-            count_words("data/test.txt")
-        },
-        "-l" => {
-            count_lines("data/test.txt")
-        },
-        "-m" => {
-            count_chars("data/test.txt")
-        }
-        }
+        "-c" => mode = Mode::Bytes,
+        "-w" => mode = Mode::Words,
+        "-l" => mode = Mode::Lines,
+        "-m" => mode = Mode::Chars,
+        str => file_name = Some(str.to_string())
     }
-    Ok(())
+  }
+
+  println!("{}", file_name);
+  Ok(())
 }
