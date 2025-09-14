5fce5173fd08b0eac9fe6d5ffccb7c171ae01c22
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-14 12:33:08 +0100

    added basic structure

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,42 @@ fn print_output(value: usize, file_name: &str) {
     println!("{} {}", value, file_name)
 }
 
+fn count_bytes(data: &str) -> usize {
+    data.len()
+}
+
+fn count_words(data: &str) -> usize {
+    1
+}
+
+fn count_lines(data: &str) -> usize {
+    1
+}
+
+fn count_chars(data: &str) -> usize {
+    1
+}
+
+
 fn main() -> Result<(), std::io::Error> {
-    let content = read_file("data/test.txt")?;
-    print_output(content.len(), "data/test.txt");
+  let args: Vec<String> = std::env::args().collect();
+
+  let mut i = 1;
+  while i < args.len() {
+    match args[i].as_str() {
+        "-c" => {
+            count_bytes("data/test.txt")
+        },
+        "-w" => {
+            count_words("data/test.txt")
+        },
+        "-l" => {
+            count_lines("data/test.txt")
+        },
+        "-m" => {
+            count_chars("data/test.txt")
+        }
+        }
+    }
     Ok(())
 }
