91f352b3255dd7009174dcd1f8c78af6d3636d42
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-15 20:22:46 +0100

    update to run mode

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,25 @@ fn read_file(path: &str) -> Result<String, std::io::Error> {
     Ok(content)
 }
 
-enum Mode { All, Bytes, Words, Lines, Chars }
+enum RunMode { All, Bytes, Words, Lines, Chars }
 
 fn main() -> Result<(), std::io::Error> {
   let args: Vec<String> = std::env::args().collect();
 
-  let mut mode = Mode::All;
+  let mut run_mode: RunMode = RunMode::All;
   let mut file_name: Option<String> = None;
 
-  let mut i = 1;
+  let mut i: usize = 1;
   while i < args.len() {
     match args[i].as_str() {
-        "-c" => mode = Mode::Bytes,
-        "-w" => mode = Mode::Words,
-        "-l" => mode = Mode::Lines,
-        "-m" => mode = Mode::Chars,
+        "-c" => run_mode = RunMode::Bytes,
+        "-w" => run_mode = RunMode::Words,
+        "-l" => run_mode = RunMode::Lines,
+        "-m" => run_mode = RunMode::Chars,
         str => file_name = Some(str.to_string())
     }
   }
 
-  println!("{}", file_name);
+  println!("{}", file_name.unwrap());
   Ok(())
 }
