e5e203b2e9be47d1b7d02727f61b2e6d36de17d8
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-17 16:00:47 +0100

    got options working

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,20 @@ fn main() -> Result<(), std::io::Error> {
 
   let mut run_mode: RunMode = RunMode::All;
   let mut file_name: Option<String> = None;
-  let content = read_file(file_name.as_deref().unwrap())?;
-
+  
   let mut i: usize = 1;
   while i < args.len() {
     match args[i].as_str() {
-        "-c" => run_mode = RunMode::Bytes,
-        "-w" => run_mode = RunMode::Words,
-        "-l" => run_mode = RunMode::Lines,
-        "-m" => run_mode = RunMode::Chars,
-        str => file_name = Some(str.to_string())
+      "-c" => run_mode = RunMode::Bytes,
+      "-w" => run_mode = RunMode::Words,
+      "-l" => run_mode = RunMode::Lines,
+      "-m" => run_mode = RunMode::Chars,
+      str => file_name = Some(str.to_string())
     }
     i += 1;
   }
+  
+  let content = read_file(file_name.as_deref().unwrap())?;
 
   let count: usize;
   match run_mode {
