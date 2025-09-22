e1a1f30eb6de372c98824e1c109b3a1657ab6fff
Jake Godsall <jake@cloudconstruct.co.uk>
2025-09-22 17:41:31 +0100

    fix read stdin to read to eof

--- src/read.rs
+++ src/read.rs
@@ -3,7 +3,7 @@ use std::io::{ Read, BufReader };
 pub fn read_stdin() -> Result<String, std::io::Error> {
   let mut content = String::new();
   std::io::stdin()
-    .read_line(&mut content)?;
+    .read_to_string(&mut content)?;
   Ok(content)
 }
 
